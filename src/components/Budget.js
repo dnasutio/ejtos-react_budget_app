0553beb88ce2e5dc2df59df1ba51317736fb4af8
Dylan Nasution <64675642+dnasutio@users.noreply.github.com>
2023-09-07 16:28:25 +0000

    budget completed and tested

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,45 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const handleChangeBudget = (newBudget) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget
+        });
+        console.log(budget);
+    }
+
+    const validateInput = (event) => {
+        const inputElement = event.target;
+        const value = inputElement.value;
+
+        if (value < totalExpenses) {
+
+        } else if ( value > budget) {
+            
+        }
+        
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    type='number' 
+                    step='10' 
+                    min={totalExpenses} 
+                    max='20000' 
+                    value={budget} 
+                    onChange={(event) => handleChangeBudget(event.target.value)}
+                    onBlur={(event) => validateInput(event)}
+                >
+                </input>
+            </span>
         </div>
     );
 }
